84e05632117a45361b392f83790f1337f45fa416
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-22 14:04:24 -0300

    add fact counter

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 // get a random dog fact from API
 fetch('https://dogapi.dog/api/v2/facts')
 .then(response => (response).json())
@@ -18,4 +20,19 @@ function updateImage(data){
 function updateFact(data){
     console.log(data.data[0].attributes.body)
     document.getElementById("fact").innerHTML = data.data[0].attributes.body
+}
+
+// get the amount of facts that were displayed by the website from the counter API
+fetch('https://api.countapi.xyz/get/codykoinabox.github.io/dogfactscounter')
+.then(response => (response).json())
+.then(data => updateCounter(data))
+
+// update the amount of cats that were displayed by the website from the counter API
+// i feel like someone will just spam this but I hope not
+fetch('https://api.countapi.xyz/update/codykoinabox.github.io/dogfactscounter/?amount=1')
+
+//update the fact amount based on the API's resonse
+function updateCounter(data){
+    console.log(data.value)
+    document.getElementById("counterValue").innerHTML = data.value
 }
